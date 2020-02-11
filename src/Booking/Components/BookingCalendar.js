c9b39da8c1cd26e4456d65300e86e347f2b1e01c
Iuliu Padurean <ciprian.padurean@gmail.com>
2020-02-11 06:06:35 -0700

    booking switched to flex-box

--- src/Booking/Components/BookingCalendar.js
+++ src/Booking/Components/BookingCalendar.js
@@ -13,189 +13,185 @@ const [disabled, active, selected] = ["calendar-date calendar-date--disabled", "
 
 
 class BookingCalendar extends Component {
-  constructor(props) {
-    super()
-    this.state = {
-      selectedMonth: new Date().getMonth(),
-      dayClicked: props.home.selectedDate,
-      start: props.home.start,
-      end: props.home.end
+    constructor(props) {
+        super()
+        this.state = {
+          selectedMonth: new Date().getMonth(),
+          dayClicked: props.home.selectedDate,
+          start: props.home.start,
+          end: props.home.end
+        }
     }
-  }
 
 
-
-
-  createMonth = () => {
+    createMonth = () => {
   
-    let now = new Date();
-    let date = new Date(now.getFullYear(), this.state.selectedMonth, 1, 0, 0, 0);
-    let select = new Date(now.getFullYear(), this.state.selectedMonth, 1, 0, 0, 0);
-        select.setTime(this.state.dayClicked);
-    
-    let daysArr = [];
-    let daysInMonth = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+        let now = new Date();
+        let date = new Date(now.getFullYear(), this.state.selectedMonth, 1, 0, 0, 0);
+        let select = new Date(now.getFullYear(), this.state.selectedMonth, 1, 0, 0, 0);
+            select.setTime(this.state.dayClicked);
+        let daysArr = [];
+        let daysInMonth = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
    
-    let d = 1;
-       for (let i=0; i<date.getDay(); i++){
-         daysArr.push(<div key={i+100} className={disabled}><div className="cal"></div></div>)
-        }
-       while (d <= daysInMonth) {
-            this.state.dayClicked && d === select.getDate()?
-                  daysArr.push( <div key={d} className={selected} data-calendar-date={date.setDate(d)} ><div className="cal">{d}</div></div>):
-            d === now.getDate() && now.getMonth() === date.getMonth()?
-                  daysArr.push( <div key={d} className={active} id="calendar-date--today" data-calendar-date={date.setDate(d)} ><div className="cal">{d}</div></div>):
-            date.setDate(d) < now.getTime()?
-                  daysArr.push( <div key={d} className={disabled} data-calendar-date={date.setDate(d)} ><div className="cal">{d}</div></div>) :
-                  daysArr.push( <div key={d} className={active} data-calendar-date={date.setDate(d)}><div className="cal">{d}</div></div>)
-            d++
-        }
-    return daysArr;
-  }
+        let d = 1;
+            for (let i=0; i<date.getDay(); i++){
+                daysArr.push(<div key={i+100} className={disabled}><div className="cal"></div></div>)
+            }
+            while (d <= daysInMonth) {
+                this.state.dayClicked && d === select.getDate()?
+                      daysArr.push( <div key={d} className={selected} data-calendar-date={date.setDate(d)} ><div className="cal">{d}</div></div>):
+                d === now.getDate() && now.getMonth() === date.getMonth()?
+                      daysArr.push( <div key={d} className={active} id="calendar-date--today" data-calendar-date={date.setDate(d)} ><div className="cal">{d}</div></div>):
+                date.setDate(d) < now.getTime()?
+                      daysArr.push( <div key={d} className={disabled} data-calendar-date={date.setDate(d)} ><div className="cal">{d}</div></div>) :
+                      daysArr.push( <div key={d} className={active} data-calendar-date={date.setDate(d)}><div className="cal">{d}</div></div>)
+                d++
+            }
+        return daysArr;
+    }
 
 
 
-  monthPrev = () => {
-    this.state.selectedMonth &&
-      this.setState(prevState => ({
-      selectedMonth: prevState.selectedMonth - 1,
-      dayClicked: false,
-      start:null,
-      end:null
-    }))
-  }
+    monthPrev = () => {
+        this.state.selectedMonth &&
+          this.setState(prevState => ({
+          selectedMonth: prevState.selectedMonth - 1,
+          dayClicked: false,
+          start:null,
+          end:null
+        }))
+    }
 
-  monthNext = () => {
-    this.setState(prevState => ({
-      selectedMonth: prevState.selectedMonth + 1,
-      dayClicked: false,
-      start:null,
-      end:null
-    }))
-  }
+    monthNext = () => {
+        this.setState(prevState => ({
+          selectedMonth: prevState.selectedMonth + 1,
+          dayClicked: false,
+          start:null,
+          end:null
+        }))
+    }
 
-  getMonthYear = () => {
-    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let date = new Date();
-    date.setMonth(this.state.selectedMonth);
-    return months[this.state.selectedMonth % 12] + " " + date.getFullYear()
-  }
+    getMonthYear = () => {
+        let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        let date = new Date();
+        date.setMonth(this.state.selectedMonth);
+        return months[this.state.selectedMonth % 12] + " " + date.getFullYear()
+    }
 
   //determining the selected date in order to display the hours belonging the that specific date
-  displayDay = (event) => {
-   event.target.parentNode.className === active &&
-        this.setState({
-            dayClicked: parseInt(event.target.parentNode.dataset.calendarDate),
-            start:null,
-            end:null
-        }) 
-  }
+    displayDay = (event) => {
+        event.target.parentNode.className === active &&
+            this.setState({
+                dayClicked: parseInt(event.target.parentNode.dataset.calendarDate),
+                start:null,
+                end:null
+            }) 
+    }
 
   //selecting the starting hour and ending hour for booking
-handleClick = (event) => {
-  let x = parseInt(event.target.dataset.val)
-    if(event.target.className === "hr"){
-      this.state.start===null?
-            this.setState({start: x, end: x+1}) :
-      (x-this.state.start)<=0 ?
-            this.setState({start:x, end: x+1}) :
-      this.state.end - this.state.start >1 ?
-            this.setState({start: x, end: x+1}):
-      (x-this.state.start)>0?
-            this.setState({end: x+1}):
-            this.setState({start:null, end:null})
-    } else {
-      this.setState({start:null, end:null})
+    handleClick = (event) => {
+        let x = parseInt(event.target.dataset.val)
+            if(event.target.className === "hr"){
+                this.state.start===null?
+                    this.setState({start: x, end: x+1}) :
+                (x-this.state.start)<=0 ?
+                    this.setState({start:x, end: x+1}) :
+                this.state.end - this.state.start >1 ?
+                    this.setState({start: x, end: x+1}):
+                (x-this.state.start)>0?
+                    this.setState({end: x+1}):
+                    this.setState({start:null, end:null})
+            } else {
+                    this.setState({start:null, end:null})
+            }
     }
-}
 
 bookRide = (event, item) => {
 
-  event.preventDefault();
-  let user = this.props.user.id
-  let driver = this.props.driver.id
-  let timeTotal = this.state.end - this.state.start
-  let date1 = new Date(new Date(this.state.dayClicked).setHours(this.state.start)).toString().slice(0, 24) + " GMT-0600 (Central Standard Time)";
-  let date2 = new Date(new Date(this.state.dayClicked).setHours(this.state.end)).toString().slice(0, 24) + " GMT-0600 (Central Standard Time)";
-  const bookingBody = {
-    user_id: user, 
-    driver_id:driver ,
-    time_booked: timeTotal,
-    start_time: date1,
-    end_time: date2,
-    total: this.props.driver.rate * timeTotal,
-    note: item.extra,
-    address: item.address, 
-    review: "",
-    rating: 4
-  }
-  if (user && driver && timeTotal && !!item.address) {
-      this.props.book(bookingBody)
-  }
+    event.preventDefault();
+    let user = this.props.user.id
+    let driver = this.props.driver.id
+    let timeTotal = this.state.end - this.state.start
+    let date1 = new Date(new Date(this.state.dayClicked).setHours(this.state.start)).toString().slice(0, 24) + " GMT-0600 (Central Standard Time)";
+    let date2 = new Date(new Date(this.state.dayClicked).setHours(this.state.end)).toString().slice(0, 24) + " GMT-0600 (Central Standard Time)";
+    const bookingBody = {
+        user_id: user, 
+        driver_id:driver ,
+        time_booked: timeTotal,
+        start_time: date1,
+        end_time: date2,
+        total: this.props.driver.rate * timeTotal,
+        note: item.extra,
+        address: item.address, 
+        review: "",
+        rating: 4
+    }
+    if (user && driver && timeTotal && !!item.address) {
+        this.props.book(bookingBody)
+    }
 }
 
 
 
 
 
-render() {
-  const { booking } = this.props
+    render() {
+        const { booking } = this.props
 
-    return (
-      <div className="grid-container">
-         {booking.booked ?
-          <Invoice trip={booking.trip} 
-                   driver={this.props.driver}
-                   reset={this.reset} /> :
-          <div className="booking-container">
-              <div className="calendar-container">
-                  <div id="myCalendar" className="calendar" >
-                      <p className="username">Select date and time: </p>
-                          <div className="calendar-header">
-                             <button onClick={this.monthPrev} className="calendar-btn" data-calendar-toggle="previous">
-                                <img src={leftArrow} alt="left"></img>
-                             </button>
-                             <div className="calendar-header__label" data-calendar-label="month">{this.getMonthYear()}</div>
-                             <button onClick={this.monthNext} className="calendar-btn" data-calendar-toggle="next">
-                                 <img src={rightArrow} alt="right"></img>
-                             </button>
-                          </div>
-                          <Week />
-                          <div onClick={this.displayDay} className="calendar-body" data-calendar-area="month">
-                                {this.createMonth()}
-                          </div>
-                  </div>
-                  <div className="day">
-                         {this.state.dayClicked &&  <Day  day={this.state.dayClicked} 
-                                                          select={this.handleClick}
-                                                          start={this.state.start}
-                                                          end={this.state.end}
-                                                          driver={this.props.driver}
-                                                     />}
-                  </div>
-              </div>
-              <div className="book">
-                  <TripForm time={this.state.end - this.state.start} 
-                            driver={this.props.driver}
-                            date={{day: this.state.dayClicked, start: this.state.start, end: this.state.end}}
-                            submit={this.bookRide} />
-              </div>
-          </div>}
-    </div> 
-  
-    )
-  }
+        return (
+            <div className="booking-container">
+                {booking.booked ?
+                <Invoice trip={booking.trip} 
+                      driver={this.props.driver}
+                      reset={this.reset} /> :
+                <div className="booking-before">
+                    <div className="calendar-container">
+                        <div id="myCalendar" className="calendar" >
+                            <p className="username">Select date and time: </p>
+                                <div className="calendar-header">
+                                    <button onClick={this.monthPrev} className="calendar-btn" data-calendar-toggle="previous">
+                                        <img src={leftArrow} alt="left"></img>
+                                    </button>
+                                    <div className="calendar-header__label" data-calendar-label="month">{this.getMonthYear()}</div>
+                                    <button onClick={this.monthNext} className="calendar-btn" data-calendar-toggle="next">
+                                        <img src={rightArrow} alt="right"></img>
+                                    </button>
+                                </div>
+                                <Week />
+                                <div onClick={this.displayDay} className="calendar-body" data-calendar-area="month">
+                                      {this.createMonth()}
+                                </div>
+                        </div>
+                        <div className="day">
+                            {this.state.dayClicked &&  <Day  day={this.state.dayClicked} 
+                                                              select={this.handleClick}
+                                                              start={this.state.start}
+                                                              end={this.state.end}
+                                                              driver={this.props.driver}
+                                                        />}
+                        </div>
+                    </div>
+                    <div className="book">
+                      <TripForm time={this.state.end - this.state.start} 
+                                driver={this.props.driver}
+                                date={{day: this.state.dayClicked, start: this.state.start, end: this.state.end}}
+                                submit={this.bookRide} />
+                    </div>
+                </div>}
+            </div> 
+        )
+    }
 }
 
 
 function mapStateToProps(state){
-  return { home: state.home , booking: state.booking, user: state.auth.user }
+    return { home: state.home , booking: state.booking, user: state.auth.user }
 }
 
 function mapDispatchToProps(dispatch){
-  return {
-    book: (obj) => dispatch(bookRide(obj))
-  }
+    return {
+        book: (obj) => dispatch(bookRide(obj))
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookingCalendar);
--- src/Booking/Components/DriverProfile.js
+++ src/Booking/Components/DriverProfile.js
@@ -9,50 +9,35 @@ import { connect } from "react-redux";
 
 
 class DriverProfile extends Component {
-  // constructor(){
-  //   super();
-  //   this.state = {
-  //     user: {}
-  //   }
-  // }
- 
-
-  componentDidMount() {
-    window.scrollTo(100, 200)
-  }  
+  
+    componentDidMount() {
+      window.scrollTo(100, 200)
+    }  
 
 
 
-  render(){
+    render(){
     // console.log(this.props)
     const { driver } = this.props
     
-      return (
+        return (
             <div className="driver-container">
-              {/* <button onClick={this.props.back} className="svg-icon">
-                  <svg width="8px" height="12px" viewBox="0 0 8 12" version="1.1" >
-                      <polygon points="7.41 1.41 6 0 0 6 6 12 7.41 10.59 2.83 6"></polygon>
-                  </svg><span> Back</span>
-              </button> */}
-             
-                  <div className="bio">
-                      <div className="photo">
-                          <div className="username">{driver.name} <h6>Chauffeur</h6></div>
-                          <img id="profile-photo" alt="img" src={driver.photo}/>
-                          <em>Rating {driver.rating}*</em>
-                      </div>
-                      <div className="description">{driver.description}</div>
-                      <div id="hourly-rate">Rate: ${driver.rate}/hour</div>
-                  </div>
+                <div className="bio">
+                    <div className="photo">
+                        <div className="username">{driver.name} <h6>Chauffeur</h6></div>
+                        <img id="profile-photo" alt="img" src={driver.photo}/>
+                        <em>Rating {driver.rating}*</em>
+                    </div>
+                    <div className="description">{driver.description}</div>
+                    <div id="hourly-rate">Rate: ${driver.rate}/hour</div>
+                </div>
                   
-                  <div className="vehicle">
-                      <div id="vehicle-model">{driver.car}</div>
-                      <img className="car-photo" alt="car" src={driver.car_photo}/>
-                  </div>
-              
-            
-              <BookingCalendar driver={driver} />
-        </div>
+                <div className="vehicle">
+                    <div id="vehicle-model">{driver.car}</div>
+                    <img className="car-photo" alt="car" src={driver.car_photo}/>
+                </div>
+                <BookingCalendar driver={driver} />
+            </div>
       )
     }
 
