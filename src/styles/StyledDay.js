6b26cc1133ce8cf95e5d9ff30fc5ae8ca397bdb8
Iuliu Padurean <ciprian.padurean@gmail.com>
2020-07-21 13:05:12 -0500

    test for mobile

--- src/styles/StyledDay.js
+++ src/styles/StyledDay.js
@@ -3,9 +3,12 @@ import styled from "styled-components";
 export const DayBar = styled.div`
   display: flex;
   flex-direction: column;
-  height: 420px;
+  height: calc(160px + 20vw);
   overflow: scroll;
-  
+  -ms-overflow-style: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `
 
 export const HourBox = styled.div`
@@ -13,13 +16,13 @@ export const HourBox = styled.div`
   align-items: center;
   justify-content: center;
   box-shadow: ${ props => props.busy ? 'none' : '1px 1px 1px 1px rgb(182, 181, 181)'};
-  font-size: 12px;
+  font-size: calc(6px + 0.5vw);
   background-color: ${ props => props.busy ? 'white' : props.selected ? '#a69d7c' : '#f2eedf'};
   color: ${ props => props.selected? 'white' : props.busy? 'silver' : 'black'};
   border: ${ props => props.busy ? '1px solid silver' : '1px solid white'};
-  width: 60px;
-  height: 26px;
-  margin: 4px;
+  width: calc(30px + 3vw);
+  height: calc(20px + 2vw);
+  margin: calc(1px + 0.2vw);
   border-radius: 10px;
   cursor: ${ props => props.busy ? 'default' : 'pointer'};
   &:hover {
@@ -28,6 +31,6 @@ export const HourBox = styled.div`
 `
 
 export const ScrollArrow = styled.img`
-  width: 40px;
+  width: calc(20px + 2vw);
   cursor: pointer;
 `
